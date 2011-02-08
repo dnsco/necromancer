e7f54d0c80ae457a7af2d5db6c959665126ed887
Dennis Collinson & Michael Sofaer <pair+dennis+msofaer@pivotallabs.com>
2011-02-07 18:11:19 -0800

    initial commit- BOOYAH!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Necromancer::Application.config.secret_token = 'd117c9367e5c846378577568514e059aa2f1caa544a7d0810969a88e2ecc3dd870ebd7effd82b0acf4d7011d308496ac7a2f82758036bb74e26c8e9ce55574c1'
