fd6e074665674ff5131244a0f3dec17f6759f322
Dennis Collinson & Michael Sofaer <pair+dennis+msofaer@pivotallabs.com>
2011-03-09 19:35:35 -0800

    driver base booting server curling out

--- spec/necro_spec.rb
+++ spec/necro_spec.rb
@@ -4,9 +4,45 @@ require 'capybara/rails'
 require 'capybara/rspec'
 require 'rails/all'
 
+require "active_support/dependencies"
+
+class Capybara::Driver::Necromancer < Capybara::Driver::Base
+  attr_reader :app, :rack_server
+  def initialize(app)
+    @app = app
+    @rack_server = Capybara::Server.new(@app)
+    @rack_server.boot
+
+  end
+
+  def visit path
+    @html = `curl -s #{rack_server.url(path)}`
+  end
+
+  def find(selector)
+    html.xpath(selector).map { |node| Capybara::Driver::Node.new(self, node) }
+  end
+
+  def html
+    Nokogiri::HTML(@html)
+  end
+end
+
+Capybara.register_driver :necromancer do |app|
+  Capybara::Driver::Necromancer.new(app)
+end
+
+Capybara.default_driver = :necromancer
+
 describe "ZOMBIES ARE COMING", :type => :acceptance do
   it "should run from zombies" do
     visit "/"
-    response.should contain("rails")
+    page.should_not have_content("sqlite3")
+  end
+
+  xit "should make JS calls" do
+    visit "/"
+    click_link "About your application’s environment"
+    page.should have_content("sqlite3")
   end
 end
